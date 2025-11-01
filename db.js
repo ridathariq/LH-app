33eb42ffa0197cc161bff816f227d6bff95fb007
ridathariq <153930862+ridathariq@users.noreply.github.com>
2025-11-01 12:51:08 +0530

    Remove MongoDB connection string from db.js
    Removed the MongoDB connection string for security reasons.

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://liyana2003clt:secret123@cluster0.ekopw.mongodb.net/LH', {
+mongoose.connect('', {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
 }).then(() => console.log("MongoDB Connected Successfully"))
 .catch(err => console.log("MongoDB Connection Failed", err));
+
 
 module.exports = mongoose;
